8fee758ed72518af72599591fd6c44ef1215f23b
Alireza <alireza@Alirezas-MacBook-Pro.local>
2024-04-27 09:57:12 +0330

    feat: add guess number

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use std::io;
+use rand::Rng;
+
+
 fn main() {
-    println!("Hello, world!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("Guess the number!{}", secret_number);
+
+    println!("Please input your guess.");
+
+    loop {
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess).expect("Failed to read line");
+
+    let guess: u32 =match guess.trim().parse() {
+        Ok(num) => num,
+        Err(_) => {
+            println!("wrong format: please enter a number");
+            continue
+        },
+    };
+
+    println!("you passed {}", guess);
+
+    if guess == secret_number {
+        println!("you win dude");
+        break;
+    }else if guess > secret_number {
+        println!("it's smaller")
+    }else{
+        println!("it's bigger")
+    }
+
+}
 }
