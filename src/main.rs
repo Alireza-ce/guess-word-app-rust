b20285bde9adb5da83e7a179ada73c33543dffa3
Alireza <alireza@Alirezas-MacBook-Pro.local>
2024-04-29 11:52:40 +0330

    feat: add guess number app

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,35 @@
-use std::io;
 use rand::Rng;
-
+use std::io;
 
 fn main() {
-
     let secret_number = rand::thread_rng().gen_range(1..=100);
-
-    println!("Guess the number!{}", secret_number);
 
     println!("Please input your guess.");
 
     loop {
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin().read_line(&mut guess).expect("Failed to read line");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    let guess: u32 =match guess.trim().parse() {
-        Ok(num) => num,
-        Err(_) => {
-            println!("wrong format: please enter a number");
-            continue
-        },
-    };
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("wrong format: please enter a number");
+                continue;
+            }
+        };
 
-    println!("you passed {}", guess);
+        println!("you passed {}", guess);
 
-    if guess == secret_number {
-        println!("you win dude");
-        break;
-    }else if guess > secret_number {
-        println!("it's smaller")
-    }else{
-        println!("it's bigger")
+        if guess == secret_number {
+            println!("you win dude");
+            break;
+        } else if guess > secret_number {
+            println!("it's smaller")
+        } else {
+            println!("it's bigger")
+        }
     }
-
-}
 }
